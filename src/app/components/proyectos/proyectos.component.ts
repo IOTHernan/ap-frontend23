2ffd243c55d24a26873077e47634635708ddab3c
HCanestraro <123335461+HCanestraro@users.noreply.github.com>
2023-11-08 00:25:24 -0300

    v5.0

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
-export class ProyectosComponent {
+export class ProyectosComponent implements OnInit{
+  miPortfolio: any;
+  constructor(private portfolioService: PortfolioService) {
+    
+  }
+  ngOnInit(): void {
+    this.portfolioService.obtenerDatos().subscribe(data => {
+      console.log(data);
+      
+      this.miPortfolio=data.proyectos;
+      console.log(this.miPortfolio);
+      
+    });
+    
+  }
 
+  selectItem(item:any) {
+    console.log(item);
+    
+  } 
 }
--- src/app/components/softskills/softskills.component.ts
+++ src/app/components/softskills/softskills.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-softskills',
   templateUrl: './softskills.component.html',
   styleUrls: ['./softskills.component.css']
 })
-export class SoftskillsComponent {
+export class SoftskillsComponent implements OnInit{
+  miPortfolio: any;
+  constructor(private portfolioService: PortfolioService) {
+    
+  }
+  ngOnInit(): void {
+    this.portfolioService.obtenerDatos().subscribe(data => {
+      console.log(data);
+      
+      this.miPortfolio=data.softskills;
+      console.log(this.miPortfolio);
+      
+    });
+    
+  }
 
+  selectItem(item:any) {
+    console.log(item);
+    
+  } 
 }
