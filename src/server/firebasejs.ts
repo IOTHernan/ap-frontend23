b2818ebf0ea29edba68e7a78d8a7c3515f6929ec
Che Tester <che.tester.ar@gmail.com>
2024-02-06 17:52:31 -0300

    0206

---
+++ src/server/firebasejs.ts
@@ -0,0 +1,8 @@
+import * as admin from 'firebase-admin';
+
+// const serviceAccount = require('path/to/firebase-adminsdk.json');
+const serviceAccount = require('src\environments\client_secret_309525837536-er5t0ivbftkvli42m9isitb700gp9950.apps.googleusercontent.com.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://tu-proyecto.firebaseio.com',
+});
